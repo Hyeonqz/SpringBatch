3a6464997cb819d108d509ce17ac9e0c8d0d7daf
jinhyeongyu <wlsgusrb78@naver.com>
2024-03-27 18:32:56 +0900

    init

---
+++ Batch_Ex1/src/main/java/org/example/batch_ex1/BatchEx1Application.java
@@ -0,0 +1,13 @@
+package org.example.batch_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchEx1Application.class, args);
+	}
+
+}
---
+++ Batch_Ex1/src/main/java/org/example/batch_ex1/domain/customer/Customer.java
@@ -0,0 +1,4 @@
+package org.example.batch_ex1.domain.customer;
+
+public class Customer {
+}
